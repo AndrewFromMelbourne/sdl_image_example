3578ff2706a2b401e6cbdb942c78048d32bdafee
Andrew <andrew@raspberrypi.(none)>
2012-11-24 00:28:43 +1100

    Just set display width and height to that of the image

--- test_image.c
+++ test_image.c
@@ -6,8 +6,6 @@ int main(int argc, char* argv[])
     SDL_Surface *screen = NULL;
 	SDL_Surface *image = NULL;
 	const SDL_VideoInfo *videoInfo = NULL;
-	int width = 0;
-	int height = 0;
 
 	/*-----------------------------------------------------------------*/
 
@@ -47,28 +45,8 @@ int main(int argc, char* argv[])
 
 	/*-----------------------------------------------------------------*/
 
-	if (image->w < videoInfo->current_w)
-	{
-		width = image->w;
-	}
-	else
-	{
-		width = videoInfo->current_w;
-	}
-
-	if (image->h < videoInfo->current_h)
-	{
-		height = image->h;
-	}
-	else
-	{
-		height = videoInfo->current_h;
-	}
-
-	/*-----------------------------------------------------------------*/
-
-    screen = SDL_SetVideoMode(width,
-							  height,
+    screen = SDL_SetVideoMode(image->w,
+							  image->h,
 							  videoInfo->vfmt->BitsPerPixel,
 							  SDL_HWSURFACE);
 
