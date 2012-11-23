d48b56d085d6db662ffa2521e77e77d822fdf988
Andrew <andrew@raspberrypi.(none)>
2012-11-24 00:32:28 +1100

    Changed tabs to spaces.

--- test_image.c
+++ test_image.c
@@ -4,67 +4,67 @@
 int main(int argc, char* argv[])
 {
     SDL_Surface *screen = NULL;
-	SDL_Surface *image = NULL;
-	const SDL_VideoInfo *videoInfo = NULL;
+    SDL_Surface *image = NULL;
+    const SDL_VideoInfo *videoInfo = NULL;
 
-	/*-----------------------------------------------------------------*/
+    /*-----------------------------------------------------------------*/
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "single argument ... name of image to display\n");
-		return 1;
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "single argument ... name of image to display\n");
+        return 1;
+    }
 
-	/*-----------------------------------------------------------------*/
+    /*-----------------------------------------------------------------*/
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		fprintf(stderr, "SDL_Init failed - %s\n", SDL_GetError());
-		return 1;
-	}
-
-	/*-----------------------------------------------------------------*/
-
-	videoInfo = SDL_GetVideoInfo();
-
-	if (videoInfo == 0)
-	{
-		fprintf(stderr, "SDL_GetVideoInfo failed - %s\n", SDL_GetError());
-    	SDL_Quit();
-	 	return 1;
-	}
-
-	/*-----------------------------------------------------------------*/
-
-	image = IMG_Load(argv[1]);
-
-	if (!image)
-	{
-		fprintf(stderr, "IMG_Load failed - %s\n", IMG_GetError());
-	}
-
-	/*-----------------------------------------------------------------*/
-
-    screen = SDL_SetVideoMode(image->w,
-							  image->h,
-							  videoInfo->vfmt->BitsPerPixel,
-							  SDL_HWSURFACE);
-
-    if (!screen)
     {
-		fprintf(stderr, "SetVideoMode failed - %s\n", SDL_GetError());
-		SDL_FreeSurface(image);
+        fprintf(stderr, "SDL_Init failed - %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /*-----------------------------------------------------------------*/
+
+    videoInfo = SDL_GetVideoInfo();
+
+    if (videoInfo == 0)
+    {
+        fprintf(stderr, "SDL_GetVideoInfo failed - %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-	/*-----------------------------------------------------------------*/
+    /*-----------------------------------------------------------------*/
 
-	SDL_BlitSurface(image, 0, screen, 0);
-	
+    image = IMG_Load(argv[1]);
+
+    if (!image)
+    {
+        fprintf(stderr, "IMG_Load failed - %s\n", IMG_GetError());
+    }
+
+    /*-----------------------------------------------------------------*/
+
+    screen = SDL_SetVideoMode(image->w,
+                              image->h,
+                              videoInfo->vfmt->BitsPerPixel,
+                              SDL_HWSURFACE);
+
+    if (!screen)
+    {
+        fprintf(stderr, "SetVideoMode failed - %s\n", SDL_GetError());
+        SDL_FreeSurface(image);
+        SDL_Quit();
+        return 1;
+    }
+
+    /*-----------------------------------------------------------------*/
+
+    SDL_BlitSurface(image, 0, screen, 0);
+    
     SDL_Delay(5000);
 
-	SDL_FreeSurface(image);
+    SDL_FreeSurface(image);
 
     SDL_Quit();
 
